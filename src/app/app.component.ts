cc284cc60d1524a4e1f029ce7ac75b780ab3ead5
Gleb Pavlov <winox@bk.ru>
2023-01-12 18:30:26 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oct-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TestOctaviangameart';
+}
